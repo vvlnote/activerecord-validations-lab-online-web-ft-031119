6867dce819fcc4c92bb3b730c6d54e6abf5d2edc
Joy Lin <vvlnote@hotmail.com>
2019-05-23 19:57:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,12 @@ class Post < ActiveRecord::Base
   
   def title_is_clickbait
     clickbait_titles = ["Won't Believe", "Secret","Top/[0..9]/", "Guess"]
-    if !!title && !title.include?("Won't Believe")
+    if !!title
+      clickbait_titles.each do |t|
+        if !title.include?(t)
+          errors.add
+      end
+      && !title.include?("Won't Believe")
       errors.add(:title_is_not_clickbait, "title is not clickbait-y!")
     end
   end
