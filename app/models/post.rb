8bbc27acaccf531aed67a208dc5cb9f899158cb1
Joy Lin <vvlnote@hotmail.com>
2019-05-23 19:54:52 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  
+  validate :title_is_clickbait
+  
+  def title_is_clickbait
+    clickbait_titles = []
+    if !!title && !title.include?("Won't Believe")
+      errors.add(:title_is_not_clickbait, "title is not clickbait-y!")
+    end
+  end
 end
